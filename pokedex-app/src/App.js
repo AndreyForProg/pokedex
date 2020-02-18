794a99333d380af7f2938dd07cd069587f245424
Andrey Pogorelchenco <53906265+AndreyForProg@users.noreply.github.com>
2020-02-19 01:41:16 +0300

    create app structure

---
+++ pokedex-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.scss'
+import {GetStartPage} from './components/GetStartedPage/GetStartedPage'
+
+function App() {
+  return (
+    <div className="App">
+      <GetStartPage />
+    </div>
+  )
+}
+
+export default App
---
+++ pokedex-app/src/components/GetStartedPage/GetStartedPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../App.scss'
+
+export const GetStartPage = () => {
+  return (
+    <div className="start_wrapper">
+        <button class="btn start_btn">Get Starting</button>
+    </div>
+  )
+}
